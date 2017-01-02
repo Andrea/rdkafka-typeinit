85274c4e556f0b88c7eaba47c1535ea05ac9dc12
Andrea Magnorsky <andrea.magnorsky@workday.com>
2017-01-02 17:26:52 +0000

    with changes

--- rdkafka/Program.fs
+++ rdkafka/Program.fs
@@ -1,8 +1,20 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
+open System
+open System.Text
+open RdKafka
+
+
 [<EntryPoint>]
 let main argv = 
-    printfn "HELLLOOOOOO %A" argv
-    0 // return an integer exit code
-
+    use p  = new Producer("127.0.0.1:2181")
+    use topic = p.Topic("Bla")
+    let datak = Encoding.UTF8.GetBytes("Hello RdKafka")
+    let result = 
+        async {
+            let! deliveryReport = topic.Produce datak |> Async.AwaitTask
+            printfn "Produced to Partition: %i, Offset: %i" deliveryReport.Partition deliveryReport.Offset
+            return deliveryReport
+            }
+    0 
